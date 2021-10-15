2bd145e462dd1f38afd05e618ebb3924c0aaa936
Julian <julian.chenard@gmail.com>
2021-10-15 22:06:11 +0200

    From Tauri to Neutralinojs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { appWindow } from '@tauri-apps/api/window'
 
 
 @Component({
@@ -11,6 +10,5 @@ export class AppComponent {
   title = 'projectmanager';
 
   ngOnInit() {
-    appWindow.maximize();
   }
 }
