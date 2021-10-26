43f323ffbef90137da01129d484d87ba4b8b71e2
Julian <julian.chenard@gmail.com>
2021-10-26 16:15:01 +0200

    Configuration updated for production

--- projectmanager-ng/src/main.ts
+++ projectmanager-ng/src/main.ts
@@ -8,24 +8,15 @@ declare let Neutralino: any;
 
 if (environment.production) {
   enableProdMode();
+
+  console.log("NEUTRALINO INIT")
+  Neutralino.init();
+
+  Neutralino.events.on("windowClose", () => {
+    Neutralino.app.exit();
+  });
+
 }
-
-console.log("NEUTRALINO INIT")
-Neutralino.init();
-
-Neutralino.events.on("windowClose", () => {
-  Neutralino.app.exit();
-});
-
-Neutralino.events.on("ready", () => {
-  console.log("ready");
-  Neutralino.os.showMessageBox('Hello', 'Welcome');
-});
-
-setTimeout(() => {
-  console.log("timeout");
-  Neutralino.os.showMessageBox('Hello', 'Welcome');
-}, 1500);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
