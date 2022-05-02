f8706d83d86ab234a09cc1c328fd309af4e9e51a
Julian <julian.chenard@gmail.com>
2022-05-02 23:14:23 +0200

    New view added : list of open actions

--- src/app/views/actions-list/actions-list.component.ts
+++ src/app/views/actions-list/actions-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Action, ActionStatus, Project } from 'src/app/model/project.model';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-actions-list',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionsListComponent implements OnInit {
 
-  constructor() { }
+  allActions: Array<{
+    color: string,
+    projectinternalid: string,
+    projectid: string,
+    action: Action
+  }> = [];
+
+  allProjects: Array<Project> = [];
+
+  constructor(
+    private db: DatabaseService,
+  ) { }
 
   ngOnInit(): void {
+
+    this.db.getProjects$().subscribe(data => {
+
+      this.allProjects = data;
+      // Removede archived projects
+      this.allProjects = this.allProjects.filter(p => p.status !== Project.STATUS.archived);
+
+      this.allProjects.map(p => {
+        // for each actions, get only actions not resolved
+        let openActions = p.actions.filter(a => a.status !== ActionStatus.CLOSED);
+        for (let action of openActions) {
+          this.allActions.push({
+            color: p.color,
+            projectinternalid: p.internalid,
+            projectid: p.id,
+            action: action
+          });
+        }
+      })
+    })
   }
 
 }
